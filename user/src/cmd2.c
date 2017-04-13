f891864c08cbcb1bc06f339f26b4147b47dd9ff4
nsalleron <nicolas.salleron@gmail.com>
2017-04-13 23:26:34 +0200

    Adding kill and modinfo from tests.c Other classics cmds should work from exec + pipe;

--- user/src/cmd2.c
+++ user/src/cmd2.c
@@ -9,8 +9,10 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <cmd/def.h>
-#define TRUE 1
+#include <sys/wait.h>
 
+#define TRUE 1
+#define TAILLE 256
 
 
 char 	*CMD_BRUT;
@@ -20,48 +22,142 @@ char 	*TEMPARG;
 int 	ARGC;
 char 	**ARGS;
 char 	shell_prompt[200];
+int 	unknown;
+char 	DATA[4*TAILLE];
+int	tube[2];
 
+void modinfo(int fd, char *module_name, int async){
+	struct cmd_params 	params;
+	struct cmd_status 	status;
+	int 			result;
+	int 			r;
+	char 			name[50];
+
+	params.asynchronous = async;
+	params.status = &status;
+	params.args.modinfo.name = malloc(sizeof(char)*strlen(module_name));
+	strcpy(params.args.modinfo.name,module_name);
+	printf("[MODINFO] Nom du module : %s\n",params.args.modinfo.name);
+	result = ioctl(fd, IOC_modinfo, &params);
+	if ( result == -1 ) {
+		perror("ioctl modinfo");
+		exit(errno);
+	}
+	printf("[MODINFO] status code: %d\n", status.code);
+	if ( status.code < 0 ){
+		printf("error\n");
+		exit(status.code);
+	}
+	printf("[MODINFO] result : %s\n",status.res.modinfo.info);
+
+}
+
+void list(int fd, int async)
+{
+	struct cmd_params 	params;
+	struct cmd_status 	status;
+	cmdid_t 		uid[10];
+	int r;
+
+	params.asynchronous = async;
+	params.status = &status;
+	params.args.list.uid = uid;
+	params.args.list.size = 10;
+	r = ioctl(fd, IOC_list, &params);
+	if (r == -1) {
+		perror("ioctl list");
+		exit(errno);
+	}
+	printf("status code: %d\n", status.code);
+	if (status.code < 0) {
+		printf("error\n");
+		exit(status.code);
+	}
+	printf("%u commands:\n", status.res.list.size);
+	for (unsigned int i = 0 ; i < status.res.list.size ; ++i)
+		printf("uid=%u\n", uid[i]);
+}
+
+void kill_fct(int fd, char **args, int async){
+
+	struct cmd_params 	params;
+	struct cmd_status 	status;
+	int 			result;
+	int 			pid;
+	int 			signal;
+	int 			r;
+
+	params.asynchronous = async;
+	params.status = &status;
+	params.args.kill.pid = atoi(args[1]);
+	params.args.kill.sig = atoi(args[0]);
+	result = ioctl(fd, IOC_kill, &params);
+	if ( result == -1 ) {
+		perror("ioctl modinfo");
+		exit(errno);
+	}
+	printf("[KILL] status code: %d\n", status.code);
+	if ( status.code < 0 ){
+		printf("[KILL] error\n");
+		exit(status.code);
+	}
+	printf("[KILL] command : %d\n",status.res.kill.status);
+}
 
 int main(int argc, char *argv[]) {
-	
-	TEMP = malloc(200*sizeof(char));
+	int fd;
+
+	fd = open("/dev/playground", O_RDWR);
+	if (fd == -1) {
+		perror("open");
+		exit(errno);
+	}
+
+
 	
 	// Configure readline to auto-complete paths when the tab key is hit.
 	rl_bind_key('\t', rl_complete);
 
-	while (TRUE) {		
+	while (TRUE) {
+		unknown = 1;
+		
+		TEMP = malloc(200*sizeof(char));
+		
 		// Create prompt string from user name and current working directory.
-		snprintf(shell_prompt, sizeof(shell_prompt), "%s$ ", getenv("USER"));
+		snprintf(shell_prompt, sizeof(shell_prompt), "%s@CustomSHELL$ ", getenv("USER"));
 
 		// Display prompt and read input (NB: input must be freed after use)...
 		CMD_BRUT = readline(shell_prompt);
-		
+
 		// Check for EOF.
-		if (!CMD_BRUT)
+		if (CMD_BRUT == NULL){
+			free(CMD_BRUT);
 			continue;
-		
+		}
+			
+
 		// Add input to history.
 		add_history(CMD_BRUT);
+		//memcpy(TEMP,CMD_BRUT,strlen(CMD_BRUT));
 		strcpy(TEMP, CMD_BRUT);
 
+
 		if(strlen(TEMP) != 0){	// Interprétation de la commande.
-			
-			printf("CMD_BRUT : %s\n",CMD_BRUT);
-			printf("TEMP : %s\n", TEMP);
-						
-			//Traitement de la chaine input
+
+
 			CMD = strtok(TEMP, "\n");
-					
+
 			if(!strcmp(CMD, "list")){
-				printf("inside list\n");
+				unknown = 0;
+				list(fd,0);
 			}
 			if(!strcmp(CMD, "fg")){
-				
-				
+				unknown = 0;
+
 				printf("inside fg\n");
 			}
 			if(!strcmp(CMD, "kill")){
-				
+				unknown = 0;
 				ARGS = malloc(sizeof(char*)*2);
 				int i = 0;
 				while ((TEMPARG = strtok(NULL, " ")) != NULL) {
@@ -69,25 +165,23 @@ int main(int argc, char *argv[]) {
 					strcpy(ARGS[i], TEMPARG);
 					i++;
 				}
-				
+
 				//Mise en place et exécution 
 				for(i = 0; i< ARGC;i++){
 					printf("%d : %s\n", i, ARGS[i]);
 				}
 				
-				
-				
-				printf("inside kill\n");
+				kill_fct(fd,ARGS,0); // SYNC pour le moment.
 			}
 			if(!strcmp(CMD, "wait")){
-				
+				unknown = 0;
 				// Récupération du nombre de paramètres pour cette commande.
 				printf("CMD_BRUT %s\n",TEMP);
 				while ((TEMPARG = strtok(NULL, " ")) != NULL) {
 					//printf("%d : %s\n", ARGC, TEMPARG);
 					ARGC++;
 				}
-				
+
 				ARGS = malloc(sizeof(char*)*ARGC);
 				//Mise en place des parémètres dans un tableau de chaine de caractères
 				int i = 0;
@@ -98,58 +192,111 @@ int main(int argc, char *argv[]) {
 					strcpy(ARGS[i], TEMPARG);
 					i++;
 				}
-				
+
 				//Mise en place et exécution 
 				for(i = 0; i< ARGC;i++){
 					printf("%d : %s\n", i, ARGS[i]);
 				}
-					
-				
+
+
 				printf("inside wait\n");
 			}
 			if(!strcmp(CMD, "meminfo")){
+				unknown = 0;
 				printf("inside meminfo\n");
 			}
 			if(!strcmp(CMD, "modinfo")){
-				printf("inside modinfo\n");
+				unknown = 0;
+				//printf("inside modinfo\n");
+
+				ARGS = malloc(sizeof(char*));
+				int i = 0;
+				while ((TEMPARG = strtok(NULL, " ")) != NULL) {
+					ARGS[i] = strdup(TEMPARG);
+					//malloc(sizeof(TEMPARG));
+					//strcpy(ARGS[i], TEMPARG);
+					i++;
+				}
+
+				//Mise en place et exécution 
+				for(i = 0; i< ARGC;i++){
+					printf("%d : %s\n", i, ARGS[i]);
+				}
+
+
+				modinfo(fd, ARGS[0], 0);
 			}
-			
+
 			if(!strcmp(CMD, "hello")){
 				
-				printf("inside hello\n");
-				
-				int fd, r;
-				struct cmd_hello_args args;
-				struct cmd_status status;
-				//Module
-				fd = open("/dev/playground", O_RDWR);
-				if (fd == -1) {
-					perror("open");
-					exit(errno);
-				}
-				
-				args.asynchronous = 0;
-				args.status = &status;	
-				args.val = 23;	
-				r = ioctl(fd, IOC_hello, &args);	//Si ioctl fonctionne
-				if (r == -1) {
-					perror("ioctl hello");
-					exit(errno);
-				}
-
-				printf("status code: %d\n", status.code);	// résultat de la cmd (handler)
-				printf("hello ret: %d\n", status.res.hello.val);	//Valeur que l'on souhaite retourner
-
-				close(fd);
-				
 			}
+			if(!strcmp(CMD, "exit")){
+				unknown = 0;
+				exit(EXIT_SUCCESS);
+			}
+			//printf("unknown : %d\n",unknown);
+			if(unknown){
+				//Exécution d'une commande classique et attente du résultat	
+				
+				ARGC = 1;
+				
+				/* On détermine le nombre d'arguments */
+				while ((TEMPARG = strtok(NULL, " ")) != NULL) {
+					ARGC++;
+				}
+				
+				ARGS = (char**) malloc(sizeof(char*)*(ARGC+3));
+				
+				//Mise en place des paramètres
+				strcpy(TEMP, CMD_BRUT);
+				TEMPARG = strtok(TEMP, " ");	//On retire le premier élement.	
+				
+				ARGS[0] = strdup(CMD);
+				int i = 1;
+				while ((TEMPARG = strtok(NULL, " ")) != NULL) {
+					ARGS[i] = strdup(TEMPARG);
+					i++;
+				}
+				ARGS[i] = NULL;
+								
+				/* pipe pour le fils */
+				pipe(tube);
+				if(fork() == 0){
+					dup2(tube[1], STDOUT_FILENO);
+					dup2(tube[1], STDERR_FILENO); 
+					close(tube[0]);
+					close(tube[1]);
+					execvp(CMD, ARGS);
+					perror("error");
+					exit(EXIT_FAILURE);
+				}
+				close(tube[1]);
+				wait(NULL); //Attente du fils
+				
+				/* Récupération du résultat */
+				while(read(tube[0], DATA, 4*TAILLE)>0){				
+					printf("%s",DATA);
+					memset(DATA, 0, 4*TAILLE);
+				}
+				close(tube[0]);
+				
+				/* Libération des arguments */
+				for(int j=0; j<i;j++){
+					free(ARGS[j]);	
+				}
+				free(ARGS);
+			}
+			
+			
 					
 		}
-		
 		// Free input.
 		free(CMD_BRUT);
-		//Libération du ARGS
-		free(ARGS);
+		//printf("CMD_BRUT\n");
+		free(TEMP);
+		//printf("TEMP\n");
+		
+		
 	}
 	
 }
