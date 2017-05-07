336d7bf4eae3d7a36c5c549d16f1f0a5ee08b0fb
λx.x <lambdax-x@users.noreply.github.com>
2017-05-07 19:11:52 +0200

    fix fg command so it cannot wait for itself

--- module/src/cmd/handlers/fg.c
+++ module/src/cmd/handlers/fg.c
@@ -8,7 +8,20 @@ int cmd_fg_handler(struct cmd_fg_args *args, struct cmd_fg_res *res)
 {
 	int r;
 	struct cmd_work *work;
+	union cmd_args *this_args;
+	struct cmd_params *this_params;
+	struct cmd_work *this_work;
 
+	/* fg command should not wait for itself
+	 */
+	this_args = container_of(args, union cmd_args, fg);
+	this_params = container_of(this_args, struct cmd_params, args);
+	this_work = container_of(this_params, struct cmd_work, params);
+	if (args->uid == this_work->uid)
+		return -EINVAL;
+
+	/* find command and register to it as a cleaner
+	 */
 	lock_cmd_works();
 	work = find_cmd_work_unsafe(args->uid);
 	if (work == NULL) {
@@ -19,6 +32,8 @@ int cmd_fg_handler(struct cmd_fg_args *args, struct cmd_fg_res *res)
 	register_cmd_work_cleaner_unsafe(work);
 	unlock_cmd_works();
 
+	/* wait for the command to terminate and copy its output
+	 */
 	flush_cmd_work(work);
 	if (args->status != NULL) {
 		r = copy_to_user(args->status,
